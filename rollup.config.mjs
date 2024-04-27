496ab6363ebcd82eb911ef7ef0841529d0666c5c
thomasskk <tkitengec@gmail.com>
2024-04-27 21:04:38 +0200

    init

---
+++ rollup.config.mjs
@@ -0,0 +1,21 @@
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+
+/** @type {import("rollup").RollupOptions} */
+export default {
+  input: 'src/index.ts',
+  plugins: [
+    typescript({
+      tsconfig: 'tsconfig.build.json',
+      outDir: 'lib',
+      rootDir: 'src',
+    }),
+    terser(),
+  ],
+  output: [
+    {
+      format: 'esm',
+      file: 'lib/index.mjs',
+    },
+  ],
+}
